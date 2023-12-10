e0ba519d381f4bdab57dd4ebd489a1dd9ec409dd
Muhammad Saqib Ilyas <msaqib@gmail.com>
2023-12-10 18:00:39 +0500

    Completed the implementation

--- app.js
+++ app.js
@@ -14,3 +14,35 @@ const date = expiresDate.getDate()
 const day = expiresDate.getDay()
 
 expires.textContent = `giveaway offer expires ${days[day]}, ${months[month]} ${date}, ${year} at ${hours}:${minutes}`
+
+setInterval(update, 1000)
+
+function update() {
+    const remaining = getTimeRemaining()
+
+    items[0].textContent = remaining.days
+    items[1].textContent = remaining.hours
+    items[2].textContent = remaining.minutes
+    items[3].textContent = remaining.seconds
+}
+
+function getTimeRemaining() {
+    const now = new Date()
+    const timems = now.getTime()
+    let remaining = expiresDate.getTime() - timems
+
+    const days = Math.floor(remaining / (24 * 60 * 60 * 1000))
+    remaining = remaining - (days * 24 * 60 * 60 * 1000)
+    const hours = Math.floor(remaining / (60 * 60 * 1000))
+    remaining = remaining - (hours * 60 * 60 * 1000)
+    const minutes = Math.floor(remaining / (60 * 1000))
+    remaining = remaining - (minutes * 60 * 1000)
+    const seconds = Math.floor(remaining / 1000)
+
+    return {
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    }
+}
